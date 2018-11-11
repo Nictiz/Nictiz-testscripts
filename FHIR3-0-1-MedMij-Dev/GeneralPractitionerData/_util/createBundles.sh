646e42935cab68a5b358f3afa465ec8f0488804a
Alexander Henket <ahenket@xs4all.nl>
2018-11-11 12:03:58 +0100

    Reactivated lines that were disabled for testing

--- FHIR3-0-1-MedMij-Dev/GeneralPractitionerData/_util/createBundles.sh
+++ FHIR3-0-1-MedMij-Dev/GeneralPractitionerData/_util/createBundles.sh
@@ -23,9 +23,9 @@ echo "Creating Composition resources from Encounters"
 java -jar ${saxon} -xsl:"createComposition.xsl" -s:createComposition.xsl inputDir1=$outputDir1
 
 echo "Creating Create TestScript resource from fixtures"
-#java -jar ${saxon} -xsl:"createTestScript.xsl" -s:createTestScript.xsl -o:../_LoadResources/medmij-gpdata-fhir3-0-1-load-resources-createupdate-xml.xml  inputDir1=$inputDir2 inputDir2=$outputDir1
+java -jar ${saxon} -xsl:"createTestScript.xsl" -s:createTestScript.xsl -o:../_LoadResources/medmij-gpdata-fhir3-0-1-load-resources-createupdate-xml.xml  inputDir1=$inputDir2 inputDir2=$outputDir1
 
 echo "Creating Delete TestScript resource from fixtures"
-#java -jar ${saxon} -xsl:"createTestScript.xsl" -s:createTestScript.xsl -o:../_ClearResources/medmij-gpdata-fhir3-0-1-clear-resources-delete-xml.xml  inputDir1=$inputDir2 inputDir2=$outputDir1
+java -jar ${saxon} -xsl:"createTestScript.xsl" -s:createTestScript.xsl -o:../_ClearResources/medmij-gpdata-fhir3-0-1-clear-resources-delete-xml.xml  inputDir1=$inputDir2 inputDir2=$outputDir1
 
 cd "$pwd"
