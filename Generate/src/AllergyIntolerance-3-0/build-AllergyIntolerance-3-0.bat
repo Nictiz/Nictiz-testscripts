2e4d5b2630c91aba147837d03b20408e96e0c5b7
Pieter Edelman <edelman@nictiz.nl>
2021-01-28 10:54:29 +0100

    Split out NTS materials to its own repo

--- Generate/src/AllergyIntolerance-3-0/build-AllergyIntolerance-3-0.bat
+++
@@ -1,5 +0,0 @@
-@setlocal enableextensions
-@echo off
-
-call ant -f ..\..\build.xml -Dproject=AllergyIntolerance-3-0 %*
-pause
--- Generate/src/GenPractData-2-0/build-GenPractData-2-0-testing.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enableextensions
-@echo off
-
-call ant -f ..\..\build.xml -Dproject=GenPractData-2-0 -Dtestscripttools.localdir=../../Nictiz-tooling-testscripts/generate
-
-pause
