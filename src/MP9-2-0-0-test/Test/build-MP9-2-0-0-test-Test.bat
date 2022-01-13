f5de58b2fd280972a4018ebecffad14c663b4fac
Jorn Duwel <duwel@nictiz.nl>
2022-01-13 11:14:07 +0100

    AdministrationAgreement set 0

--- src/MP9-2-0-0-test/Test/build-MP9-2-0-0-test-Test.bat
+++ src/MP9-2-0-0-test/Test/build-MP9-2-0-0-test-Test.bat
@@ -1,6 +1,6 @@
 @setlocal enableextensions
 @echo off
 
-call ant -f ..\..\..\build.xml -propertyfile build.properties %*
+call ant -f ..\..\..\build.xml -propertyfile build.properties %* -Dtestscripttools.localdir=C:\Users\144189-ADM\Documents\Git\Nictiz-tooling-testscripts\generate
 
 pause
