09018ff0074e0c03719094f21f58fa1b7e0cd801
Pieter Edelman <edelman@nictiz.nl>
2022-04-14 16:46:05 +0200

    MM-3099: Remove source materials for Questionnaires 1.0, which is deprecated

--- src/Questionnaires-1-0-0/Test/build-Questionnaires-1-0-0-Test.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enableextensions
-@echo off
-
-call ant -f ..\..\build-nts-single.xml -propertyfile build.properties %*
-
-pause
--- src/Questionnaires-1-0-0/build-Cert-Test.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enabledelayedexpansion
-@echo off
-
-ant -f ..\build-nts-multiple.xml -Dinput.dirs="src/Questionnaires-2-0/Test,src/Questionnaires-2-0/Cert" %*
-
-pause
