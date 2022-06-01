53acb4c4d350dab108c12bd5c8325525949ca1eb
Pieter Edelman <edelman@nictiz.nl>
2022-06-01 19:02:45 +0200

    Add a centralizeLoadResources step using the new setup

---
+++ buildscripts/MedMij-2020.01-complete.bat
@@ -0,0 +1,7 @@
+@setlocal enabledelayedexpansion
+@echo off
+
+call ant -f ..\src\build-nts-multiple.xml -propertyfile MedMij-2020.01.properties 
+call ant -f ..\src\build-centralizeLoadResources.xml -propertyfile MedMij-2020.01.properties
+
+pause
--- buildscripts/MedMij-2020.02-complete.bat
+++ buildscripts/MedMij-2020.02-complete.bat
@@ -2,5 +2,6 @@
 @echo off
 
 call ant -f ..\src\build-nts-multiple.xml -propertyfile MedMij-2020.02.properties 
+call ant -f ..\src\build-centralizeLoadResources.xml -propertyfile MedMij-2020.02.properties
 
 pause
