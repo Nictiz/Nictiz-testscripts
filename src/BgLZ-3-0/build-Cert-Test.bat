3477f0f687cde92dc4e201db0d8172c51457ad4d
Pieter Edelman <edelman@nictiz.nl>
2022-06-01 15:44:34 +0200

    Update all batch build files in the project folders to thin wrappers around the ANT call

--- src/BgLZ-3-0/build-Cert-Test.bat
+++ src/BgLZ-3-0/build-Cert-Test.bat
@@ -1,15 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-for /F %%i IN ('cd') do (
-   set "project=%%~nxi"
-)
-for /d %%i in (%cd%\*) do (
-   set "folder=%%~nxi"
-   cd "%%i"
-   echo --- !folder! ---
-   echo | call "build-!project!-!folder!.bat"
-   cd ..
-)
+ant -f ..\build-nts-multiple.xml -Dinput.dirs="src/BgLZ-3-0/Test,src/BgLZ-3-0/Cert" %*
 
 pause
