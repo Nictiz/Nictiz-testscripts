7ee3c8dfbcd6954f815d2db55936078a3449cbcf
Pieter Edelman <edelman@nictiz.nl>
2022-06-02 12:07:35 +0200

    Add narrative generation to the build

--- buildscripts/MedMij-2020.01-complete.bat
+++ buildscripts/MedMij-2020.01-complete.bat
@@ -3,5 +3,6 @@
 
 call ant -f ..\src\build-nts-multiple.xml -propertyfile MedMij-2020.01.properties 
 call ant -f ..\src\build-centralizeLoadResources.xml -propertyfile MedMij-2020.01.properties
+call ant -f ..\src\build-addNarratives.xml -propertyfile MedMij-2020.01.properties
 
 pause
