1141e07b9e77224c48d2214dc0bc36189c735b45
Pieter Edelman <edelman@nictiz.nl>
2022-06-02 14:45:04 +0200

    Migrate MedMij builds to the new build scripts

--- src/AllergyIntolerance-3-0/build-Cert-Test.bat
+++ src/AllergyIntolerance-3-0/build-Cert-Test.bat
@@ -1,5 +1,5 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-ant -f ..\build-nts-multiple.xml -Dinput.dirs="src/AllergyIntolerance-3-0/Test,src/AllergyIntolerance-3-0/Cert" %*
+ant -f ..\build-multiple.xml -propertyfile build.properties %*
 pause
