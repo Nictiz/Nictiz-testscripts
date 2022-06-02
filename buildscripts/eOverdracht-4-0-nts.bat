6e94830562d8685e9dc4996e0c3378c9dfad7204
Pieter Edelman <edelman@nictiz.nl>
2022-06-02 12:07:35 +0200

    Add narrative generation to the build

--- buildscripts/eOverdracht-4-0-nts.bat
+++ buildscripts/eOverdracht-4-0-nts.bat
@@ -2,5 +2,6 @@
 @echo off
 
 call ant -f ..\src\build-nts-multiple.xml -propertyfile eOverdracht-4.0.properties 
+call ant -f ..\src\build-addNarratives.xml -propertyfile eOverdracht-4.0.properties
 
 pause
