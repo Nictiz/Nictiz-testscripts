ef49a77974e7a3b23f172654346fd45c12e0d832
Arianne van de Wetering <wetering@nictiz.nl>
2022-06-28 11:22:27 +0200

    remove local reference from .bat

--- src/MP9-2-0-0/Test/build-MP9-2-0-0-Test.bat
+++ src/MP9-2-0-0/Test/build-MP9-2-0-0-Test.bat
@@ -1,6 +1,6 @@
 @setlocal enableextensions
 @echo off
 
-call ant -f ..\..\..\build.xml -propertyfile build.properties %* -Dtestscripttools.localdir=C:\Users\144189-ADM\Documents\Git\Nictiz-tooling-testscripts\generate
-
+rem call ant -f ..\..\..\build.xml -propertyfile build.properties %* -Dtestscripttools.localdir=C:\Users\144189-ADM\Documents\Git\Nictiz-tooling-testscripts\generate
+call ant -f ..\..\..\build.xml -propertyfile build.properties %*
 pause
