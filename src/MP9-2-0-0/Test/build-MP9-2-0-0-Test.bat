58f5b3edcbd70fb8df60ee3c05a15270b11d5442
Jorn Duwel <duwel@nictiz.nl>
2022-07-28 11:36:05 +0200

    Get build working (again)

--- src/MP9-2-0-0/Test/build-MP9-2-0-0-Test.bat
+++ src/MP9-2-0-0/Test/build-MP9-2-0-0-Test.bat
@@ -1,10 +1,6 @@
 @setlocal enableextensions
 @echo off
 
-rem local Jorn dir
-rem call ant -f ..\..\..\build.xml -propertyfile build.properties %* -Dtestscripttools.localdir=C:\Users\144189-ADM\Documents\Git\Nictiz-tooling-testscripts\generate
-rem local Arianne dir
-call ant -f ..\..\..\build.xml -propertyfile build.properties %* -Dtestscripttools.localdir=C:\SourceTree\GitHub\Nictiz-tooling-testscripts\generate
+call ant -f ..\..\..\build.xml -propertyfile build.properties %*
 
-rem call ant -f ..\..\..\build.xml -propertyfile build.properties %*
 pause
