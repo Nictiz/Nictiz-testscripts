8365f96d4812e6f6424d66149e575b5624fde9b8
Jorn Duwel <duwel@nictiz.nl>
2022-09-26 15:03:36 +0200

    HOTFIX: Add target to id and url of internal scripts to make them uniquely identifyable

--- dev/FHIR3-0-2-MM202001-Cert/Questionnaires-2-0/_reference/rules/modifyTask.groovy
+++
@@ -1,7 +0,0 @@
-/*
- rule.summary=Modify the Task from a responseId with a different status
- rule.description=Modify the Task from a responseId with a different status
- rule.param.newStatus.required=true
-*/
-
-output[param.taskModified] = "Foo bar"
