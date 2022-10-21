b06446ae641d216e7672ea27d637969bc4f9c46b
Pieter Edelman <edelman@nictiz.nl>
2022-10-21 17:10:36 +0200

    Mimimal working example of a rule producing output

---
+++ src/Questionnaires-2-0/Cert/_reference/rules/modifyTask.groovy
@@ -0,0 +1,7 @@
+/*
+ rule.summary=Modify the Task from a responseId with a different status
+ rule.description=Modify the Task from a responseId with a different status
+ rule.param.newStatus.required=true
+*/
+
+output["taskModified"] = "Foo bar"
