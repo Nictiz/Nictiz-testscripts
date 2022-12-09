b98131ff960928ce3a706bd251d414826a0f25de
Onno <onno.gieling@gmail.com>
2022-12-09 16:19:42 +0100

    Remove imm buildscripts

--- buildscripts/Immunisatie-0-1-0-complete.bat
+++
@@ -1,8 +0,0 @@
-@setlocal enabledelayedexpansion
-@echo off
-
-call ant -f ..\src\build-nts-multiple.xml -propertyfile Immunisatie-0-1.properties
-call ant -f ..\src\build-centralizeLoadResources.xml -propertyfile Immunisatie-0-1.properties 
-call ant -f ..\src\build-addNarratives.xml -propertyfile Immunisatie-0-1.properties
-
-pause
