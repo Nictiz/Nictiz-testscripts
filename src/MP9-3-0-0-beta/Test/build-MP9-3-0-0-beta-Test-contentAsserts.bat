bf2985a903a03923af521baa31d3fb69b1f58e73
Jorn Duwel <duwel@nictiz.nl>
2023-03-09 17:07:31 +0100

    Create test, unhappy script and proposal for KT-360

--- src/MP9-3-0-0-beta/Test/build-MP9-3-0-0-beta-Test-contentAsserts.bat
+++ src/MP9-3-0-0-beta/Test/build-MP9-3-0-0-beta-Test-contentAsserts.bat
@@ -3,4 +3,6 @@
 
 call ant -f  ..\..\build-nts-single.xml -propertyfile build-contentAsserts.properties %*
 
+ren ..\..\..\dev\FHIR4-0-1-Test\MP9-3-0-0-beta\contentAsserts\MedicationData\Serve\AdministrationAgreement-contentAsserts AdministrationAgreement
+
 pause
