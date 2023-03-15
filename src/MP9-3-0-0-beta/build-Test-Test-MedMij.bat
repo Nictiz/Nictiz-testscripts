54f1608b234f8ea1e357c8e4d78653df0d4643a1
Jorn Duwel <duwel@nictiz.nl>
2023-03-15 13:50:28 +0100

    Fix MTD ReasonForDeviation fixtures

--- src/MP9-3-0-0-beta/build-Test-Test-MedMij.bat
+++ src/MP9-3-0-0-beta/build-Test-Test-MedMij.bat
@@ -1,5 +1,11 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\build-multiple.xml -propertyfile build.properties %*
+rem call ant -f ..\build-multiple.xml -propertyfile build.properties %*
+rem calling separate build scripts, otherwise narrative generation does not work for MedMij target
+cd Test
+@echo | call build-MP9-3-0-0-beta-Test.bat
+cd ..\Test-MedMij
+call build-MP9-3-0-0-beta-Test-MedMij.bat
+
 pause
