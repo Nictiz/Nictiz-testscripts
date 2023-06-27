3e1ae8d07839b4ce194205180bd14784313f8888
Pieter Edelman <edelman@nictiz.nl>
2023-06-27 23:06:24 +0200

    Bugfixes and documentation for the Task rewrite rule

--- src/common-components/_reference/rules/modifyTask.groovy
+++ src/common-components/_reference/rules/modifyTask.groovy
@@ -1,47 +1,50 @@
 /*
- rule.summary=Modify the Task from a responseId with a different status
- rule.description=Modify the Task from a responseId with a different status
+ rule.summary=Create new Task fixture
+ rule.description=Create a new Task fixture based on the retreived Task for later use in the TestScript. This is not an actual test, but a step needed for the TestScript infrastructure.
  rule.param.newStatus.required=true
  rule.param.fixtureId.required=true
+ rule.param.responseId.required=false
 */
 
 import groovy.xml.XmlUtil
 
-def isJson = (response.body.bodyContentTypeEnum.toString() == 'json')
+def usedResponse = (param.responseId == null) ? response : responses.get(param.responseId)
+def isJson = (usedResponse.body.bodyContentTypeEnum.toString() == 'json')
 
 // Let's get our hands on the Task resource from the response. Since this Groovy rule is not trivial to write and
 // maintain, we want to support both the read and the search use case with the same rule. The search use case assumes
 // that there the response is a search Bundle containing exactly one Task resource.
-
 def task = null
-if (response.resource == "Task") {
-    task = response.body
-} else if (response.resource == "Bundle") {
-    assert response.getFhirPathBoolean("Bundle.entry.where(resource is Task).count() = 1"): "Server didn't return a Bundle with a single Task."
+if (usedResponse.resource == "Task") {
+    task = usedResponse.body.document
+} else if (usedResponse.resource == "Bundle") {
+    assert usedResponse.getFhirPathBoolean("Bundle.entry.where(resource is Task).count() = 1"): "Server didn't return a Bundle with a single Task."
 
     if (isJson) {
-        for (entry in response.body.document.entry) {
+        for (entry in usedResponse.body.document.entry) {
             if (entry.resource.resourceType == "Task") {
                 task = entry.resource
             }
         }
     } else {
-        task = response.body.document.entry.'**'.find { it.name() == "Task" }
+        task = usedResponse.body.document.entry.'**'.find { it.name() == "Task" }
     }
 }
 assert task != null : "Couldn't extract Task resource from response."
 
+// Now modify the Task status and write the result to the output. We also remove the narrative as there is no guarantee
+// anymore that it still complies to the Task.
 if (isJson) {
-    task.status = '${param.newStatus}'
+    task.status = param.newStatus
     if ("text" in task) {
         task.remove("text")
     }
 
-    output['${param.fixtureId}'] = JsonOutput.toJson(task)
+    output[param.fixtureId] = JsonOutput.toJson(task)
 } else {
-    task.status.@value = '${param.newStatus}'
+    task.status.@value = param.newStatus
     if (task.text != null) {
         task.text.replaceNode {}
     }
-    output['${param.fixtureId}'] = XmlUtil.serialize(task)
+    output[param.fixtureId] = XmlUtil.serialize(task)
 }
