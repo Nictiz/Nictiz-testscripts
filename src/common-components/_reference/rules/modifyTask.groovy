f61d547907c82caff3e0096cdc7f2289b084528a
Pieter Edelman <edelman@nictiz.nl>
2023-06-27 21:41:15 +0200

    Let the rule handle both search and read operations for the Task (untested).
    In addition, remove the narrative as we cannot guarantee it still complies to the content of the Task.

--- src/common-components/_reference/rules/modifyTask.groovy
+++ src/common-components/_reference/rules/modifyTask.groovy
@@ -7,35 +7,41 @@
 
 import groovy.xml.XmlUtil
 
-assert response.resource == "Bundle": "Server didn't return a Bundle with a single Task."
-assert response.getFhirPathBoolean("Bundle.entry.where(resource is Task).count() = 1"): "Server didn't return a Bundle with a single Task."
-
 def isJson = (response.body.bodyContentTypeEnum.toString() == 'json')
 
-logger.info(response.body.bodyContentTypeEnum.toString())
+// Let's get our hands on the Task resource from the response. Since this Groovy rule is not trivial to write and
+// maintain, we want to support both the read and the search use case with the same rule. The search use case assumes
+// that there the response is a search Bundle containing exactly one Task resource.
 
 def task = null
-if (isJson) {
-    for (entry in response.body.document.entry) {
-        if (entry.resource.resourceType == "Task") {
-            task = entry.resource
-        }
-    }
-} else {
-    task = response.body.document.entry.'**'.find { it.name() == "Task" }
-}
+if (response.resource == "Task") {
+    task = response.body
+} else if (response.resource == "Bundle") {
+    assert response.getFhirPathBoolean("Bundle.entry.where(resource is Task).count() = 1"): "Server didn't return a Bundle with a single Task."
 
+    if (isJson) {
+        for (entry in response.body.document.entry) {
+            if (entry.resource.resourceType == "Task") {
+                task = entry.resource
+            }
+        }
+    } else {
+        task = response.body.document.entry.'**'.find { it.name() == "Task" }
+    }
+}
 assert task != null : "Couldn't extract Task resource from response."
 
 if (isJson) {
     task.status = '${param.newStatus}'
+    if ("text" in task) {
+        task.remove("text")
+    }
+
     output['${param.fixtureId}'] = JsonOutput.toJson(task)
 } else {
     task.status.@value = '${param.newStatus}'
+    if (task.text != null) {
+        task.text.replaceNode {}
+    }
     output['${param.fixtureId}'] = XmlUtil.serialize(task)
 }
-
-// task.text = ''
-// if (response.body.bodyContentTypeEnum == 'json') {
-    
-// }
