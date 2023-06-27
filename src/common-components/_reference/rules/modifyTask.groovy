765550e077d71b0ef254782b81e127f5ed785e5b
Pieter Edelman <edelman@nictiz.nl>
2023-06-27 20:50:07 +0200

    Let the modifyTask script handle both XML and JSON.

--- src/common-components/_reference/rules/modifyTask.groovy
+++ src/common-components/_reference/rules/modifyTask.groovy
@@ -5,19 +5,37 @@
  rule.param.fixtureId.required=true
 */
 
-assert response.resource == "Bundle": "Server didn't return a Bundle with a single Task."
-def bundle = response.body.document
+import groovy.xml.XmlUtil
 
+assert response.resource == "Bundle": "Server didn't return a Bundle with a single Task."
 assert response.getFhirPathBoolean("Bundle.entry.where(resource is Task).count() = 1"): "Server didn't return a Bundle with a single Task."
+
+def isJson = (response.body.bodyContentTypeEnum.toString() == 'json')
+
+logger.info(response.body.bodyContentTypeEnum.toString())
+
 def task = null
-for (entry in bundle.entry) {
-    if (entry.resource.resourceType == "Task") {
-        task = entry.resource
+if (isJson) {
+    for (entry in response.body.document.entry) {
+        if (entry.resource.resourceType == "Task") {
+            task = entry.resource
+        }
     }
+} else {
+    task = response.body.document.entry.'**'.find { it.name() == "Task" }
 }
 
-task.status = '${param.newStatus}'
+assert task != null : "Couldn't extract Task resource from response."
+
+if (isJson) {
+    task.status = '${param.newStatus}'
+    output['${param.fixtureId}'] = JsonOutput.toJson(task)
+} else {
+    task.status.@value = '${param.newStatus}'
+    output['${param.fixtureId}'] = XmlUtil.serialize(task)
+}
+
 // task.text = ''
 // if (response.body.bodyContentTypeEnum == 'json') {
-    output['${param.fixtureId}'] = JsonOutput.toJson(task)
+    
 // }
