e95e15a9687a5291eab1217459823ee92c598f2f
Jorn Duwel <duwel@nictiz.nl>
2023-08-04 13:12:54 +0200

    MM-4887: Remove assert on Reference.reference that is being handled by the Touchstone validator

---
+++ buildscripts/build-all.bat
@@ -0,0 +1,10 @@
+@echo OFF
+
+@echo | call BgZ-MSZ-1-0-complete.bat
+@echo | call eOverdracht-4-0-complete.bat
+@echo | call FHIR4-0-1-complete.bat
+@echo | call Geboortezorg-3-2-complete.bat
+@echo | call MedMij-2019.01-complete.bat
+@echo | call MedMij-2020.01-complete.bat
+@echo | call MedMij-2020.02-complete.bat
+@echo | call MedMij-V6-complete.bat
