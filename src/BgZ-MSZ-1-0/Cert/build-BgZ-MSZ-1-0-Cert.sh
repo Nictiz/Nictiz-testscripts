2cbac0d7637fef8cc3751583f8a89a3b664d5d88
Alexander Henket <henket@nictiz.nl>
2024-02-10 14:03:40 +0100

    Replace the manual copy with a script step

--- src/BgZ-MSZ-1-0/Cert/build-BgZ-MSZ-1-0-Cert.sh
+++ src/BgZ-MSZ-1-0/Cert/build-BgZ-MSZ-1-0-Cert.sh
@@ -1,4 +1,8 @@
 #@setlocal enableextensions
 #@echo off
+PWD=`dirname $0`
 
-ant -f ../../build-single.xml -propertyfile build.properties $*
+rm -f "$PWD/_reference/resources/*.xml"
+cp `readlink -f "$PWD/../../../../HL7-mappings/ada_2_fhir/bgz_msz/1.2/beschikbaarstellen/fhir_instance/"`/*.xml "$PWD/_reference/resources/"
+
+ant -f $PWD/../../build-single.xml -propertyfile $PWD/build.properties $*
