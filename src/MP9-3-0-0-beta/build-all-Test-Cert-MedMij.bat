1c4db0079fbdc9a3d40ac1c898a6efd93d3f398a
Pieter Edelman <edelman@nictiz.nl>
2025-04-18 09:23:47 +0200

    Adjust MP9 3.0.0-beta.3 to the new situation

--- src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.bat
+++ src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.bat
@@ -1,18 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-rem call ant -f ..\build-multiple.xml -propertyfile build.properties %*
-rem calling separate build scripts, otherwise narrative generation does not work for MedMij target
-cd Test
-@echo | call build-MP9-3-0-0-beta-Test.bat
-
-cd ..\Test-MedMij
-@echo | call build-MP9-3-0-0-beta-Test-MedMij.bat
-
-cd ..\Cert
-@echo | call build-MP9-3-0-0-beta-Cert.bat
-
-cd ..\Cert-MedMij
-@echo | call build-MP9-3-0-0-beta-Cert-MedMij.bat
+call ant -f ..\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij" %*
 
 pause
--- src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.sh
+++ src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 export JAVA_HOME=`/usr/libexec/java_home`
 
-echo "test-medmij"
-cd Test-MedMij
-./build-MP9-3-0-0-beta-Test-MedMij.sh
-
-echo "test"
-cd ../Test
-./build-MP9-3-0-0-beta-Test.sh
-
-echo "cert-medmij"
-cd ../Cert-MedMij
-./build-MP9-3-0-0-beta-Cert-MedMij.sh
-
-echo "cert"
-cd ../Cert
-./build-MP9-3-0-0-beta-Cert.sh
-
+ant -f ../build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij" $*
