f467b6c5ae202eb3007defdfb99ddea8eadaf607
Pieter Edelman <edelman@nictiz.nl>
2025-04-22 13:45:20 +0200

    Refine MP9 build scripts

--- src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.bat
+++ src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.bat
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij" %*
+call ant -f ..\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert-MedMij" %*
 
 pause
--- src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.sh
+++ src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export JAVA_HOME=`/usr/libexec/java_home`
 
-ant -f ../build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij" $*
+ant -f ../build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert-MedMij" $*
