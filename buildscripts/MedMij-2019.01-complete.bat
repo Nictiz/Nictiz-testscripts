ee0553ece9d8864fa53de9b171d71639750bdb5b
Lauri Rondolo <lauri.rondolo@nictiz.nl>
2025-05-23 12:13:56 +0200

    Revert "NICTIZ-31367 build-all-Test-Cert.bat"
    This reverts commit ae412644429561803b95148de2e3ef82bb55179b.

--- buildscripts/MedMij-2019.01-complete.bat
+++ buildscripts/MedMij-2019.01-complete.bat
@@ -2,6 +2,6 @@
 @echo off
 
 call ant -f ..\src\build-multiple.xml -propertyfile MedMij-2019.01.properties 
-call ant -f ..\src\build-centralizeLoadResources.xml -propertyfile MedMij-2019.01.properties
+REM call ant -f ..\src\build-centralizeLoadResources.xml -propertyfile MedMij-2019.01.properties
 
 pause
