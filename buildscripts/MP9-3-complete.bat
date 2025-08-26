3e5de1914f7facd390b7e0ab97cc1cdc4dcf009e
hvroege-nictiz <huib.vroege@nictiz.nl>
2025-08-26 14:54:36 +0200

    Generate testscripts

--- buildscripts/MP9-3-complete.bat
+++ buildscripts/MP9-3-complete.bat
@@ -1,18 +1,7 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-@REM call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij"
-echo.ant MP9-3-0-0-beta_Test...
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test" >MP9-3-0-0-beta_Test.log
-
-echo.ant MP9-3-0-0-beta_Test-MedMij...
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test-MedMij" >MP9-3-0-0-beta_Test-MedMij.log
-
-echo.ant MP9-3-0-0-beta_Cert...
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij" >MP9-3-0-0-beta_Cert.log
-
-echo.ant MP9-3-0-0-beta_Cert-MedMij...
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Cert-MedMij" >MP9-3-0-0-beta_Cert-MedMij.log
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij"
 
 echo.Done
 pause
