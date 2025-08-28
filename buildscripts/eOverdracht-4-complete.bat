01aaf1eb7d0091fe6604a55b19b646e05aa70f96
Pieter Edelman <edelman@nictiz.nl>
2025-08-28 11:57:16 +0200

    Update build-all script for inclusion of eOverdracht HeO

--- buildscripts/eOverdracht-4-complete.bat
+++ buildscripts/eOverdracht-4-complete.bat
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/eOverdracht-4-0/Test,src/eOverdracht-4-0/Cert,src/eOverdracht-4-1/Test"
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/eOverdracht-4-0/Test,src/eOverdracht-4-0/Cert,src/eOverdracht-4-1/Test,src/eOverdracht-HeO/Test"
 
 pause
