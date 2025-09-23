ed088cb422ef20aca16f40636c20600178abbfcf
Jorn Duwel <jorn@interoplab.eu>
2025-09-23 15:15:39 +0200

    CL-271/MM-5668: Move AllergyIntolerance-3-0, BgLZ-3-0, GenPractData-2-0, GGZ-2-0, Images-2-0, LaboratoryResults-2-0, Medication-9-0-7 and PatientCorrections-1-0 TEST folders to archive, edit buildscripts, rename MP-9-0-7-test/Test to MP-9-0-7/Test

--- buildscripts/MedMij-2019.01-complete.bat
+++ buildscripts/MedMij-2019.01-complete.bat
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/Medication-9-0-7/Test, src/Medication-9-0-7/Cert, src/Medication-9-0-7-test/Test"
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/Medication-9-0-7/Test, src/Medication-9-0-7/Cert"
 
 pause
--- src/GGZ-2-0/build-Cert-Test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ant -f ../build-multiple.xml -Dinput.dirs="src/GGZ-2-0/Test, src/GGZ-2-0/Cert" $*
