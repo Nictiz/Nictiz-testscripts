4c3aa420d8d2d0b2e5214775678872d8c13bfbe5
Jorn Duwel <jorn@interoplab.eu>
2026-01-06 09:52:41 +0100

    Revert temp build edits

--- src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.sh
+++ src/MP9-3-0-0-beta/build-all-Test-Cert-MedMij.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ant -f ../build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert-MedMij" -Dbuild-CLPropertiesFiles.skip=true -DgenerateTestScriptsForProjects.tool.version=4.0.0 $*
+ant -f ../build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert-MedMij" $*
