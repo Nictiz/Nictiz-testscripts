75a7b8fbd25b0da4088fc31b9322e7b097eb97ef
Evelyn Lai <evelyn.lai@nictiz.nl>
2026-01-08 19:26:01 +0100

    NICTIZ\Nictiz-testscripts\buildscripts\MP9-3-complete.bat aangepast, zodat het log bestanden maakt

--- buildscripts/MP9-3-complete.bat
+++ buildscripts/MP9-3-complete.bat
@@ -1,6 +1,19 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij"
+@REM call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test, src/MP9-3-0-0-beta/Test-MedMij, src/MP9-3-0-0-beta/Cert, src/MP9-3-0-0-beta/Cert-MedMij"
+echo.ant mp9 30 MP9-3-build Test...
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test" >MP9-3-0-0-beta_Test.log 
+
+echo.ant mp9 30 MP9-3-build Test-MedMij...
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Test-MedMij" >MP9-3-0-0-beta_Test-MedMij.log
+
+echo.ant mp9 30 MP9-3-build Cert...
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Cert" >MP9-3-0-0-beta_Cert.log
+
+echo.ant mp9 30 MP9-3-build Cert-MedMij...
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/MP9-3-0-0-beta/Cert-MedMij" >MP9-3-0-0-beta_Cert-MedMij.log
+
+echo.Done
 
 pause
