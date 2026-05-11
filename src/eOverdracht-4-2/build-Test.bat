c3d96de2e5b45ce3f7d1faeb820bfabc3dfa1b6f
Daniel Püttmann <daniel.puttmann@nictiz.nl>
2026-05-11 14:31:52 +0200

    Revert "Merge branch 'main' into NICTIZ-41406-prevalidatie"
    This reverts commit 43c04f3ef353e6bb5d17da1a4e7f8e56e2b370ed, reversing
    changes made to 9d12d8fc5e189120c45c8754a512890e5158e237.

--- src/eOverdracht-4-2/build-Test.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enabledelayedexpansion
-@echo off
-
-call ant -f ..\build-multiple.xml -Dinput.dirs="src/eOverdracht-HeO/Test" %*
-
-pause
--- src/eOverdracht-4-2/build-Test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ant -f ../build-multiple.xml -Dinput.dirs="src/eOverdracht-HeO/Test" $*
