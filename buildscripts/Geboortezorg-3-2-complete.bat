4f8b3786ac352872ec0613dbecd5e4176952451c
lisaDekker-nictiz <lisa.dekker@nictiz.nl>
2026-07-24 13:52:21 +0200

    Archive old Geboortezorg material

--- buildscripts/Geboortezorg-3-2-complete.bat
+++
@@ -1,6 +0,0 @@
-@setlocal enabledelayedexpansion
-@echo off
-
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/Geboortezorg-3-2/Cert"
-
-pause
--- buildscripts/Geboortezorg-3-2-complete.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-ant -f ../src/build-multiple.xml -Dinput.dirs="src/Geboortezorg-3-2/Cert"
