27e80bdac88c88aeed7162dbe5e19eec314a9ff9
lisaDekker-nictiz <lisa.dekker@nictiz.nl>
2026-08-11 13:55:16 +0200

    update version

--- buildscripts/BirthCare-complete.bat
+++ buildscripts/BirthCare-complete.bat
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/BirthCare/3.0/PregnancyCard/Ultrasound/Test,src/BirthCare/3.0/PregnancyCard/MaternityCare/Test"
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/BirthCare/4.0/PregnancyCard/Ultrasound/Test,src/BirthCare/4.0/PregnancyCard/MaternityCare/Test"
 
 pause
--- buildscripts/BirthCare-complete.sh
+++ buildscripts/BirthCare-complete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ant -f ../src/build-multiple.xml -Dinput.dirs="src/BirthCare/3.0/PregnancyCard/Ultrasound/Test,src/BirthCare/3.0/PregnancyCard/MaternityCare/Test"
+ant -f ../src/build-multiple.xml -Dinput.dirs="src/BirthCare/4.0/PregnancyCard/Ultrasound/Test,src/BirthCare/4.0/PregnancyCard/MaternityCare/Test"
