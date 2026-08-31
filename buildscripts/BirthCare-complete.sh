97e2f8e5dbff0502d137bb1345ab56912a402c02
lisaDekker-nictiz <lisa.dekker@nictiz.nl>
2026-08-31 08:39:42 +0200

    NICTIZ-33768 GZ actualisatie VLK casus 1 (#527)
    * add dataset for MaternityCare and reorder dataset for ultrasound
    
    * first setup of maternity care scripts
    
    * update scripts to new version testdata
    
    * filename consistency
    
    * update property name maternity care
    
    * update patient id's
    
    * update observations
    
    * added content asserts
    
    * fixes contentasserts
    
    * fix contentassert descriminator with date
    
    * fix content asserts discriminator
    
    * test fix contentassert descriminator
    
    * test 2 fix content assert descriminator
    
    * test 3 fix content assert descriminator
    
    * test 4 fix content assert descriminator
    
    * NICTIZ-35513 update maternity care data fixtures
    
    * NICTIZ-35513 update references and descriminator encounter
    
    * Merge branch 'NICTIZ-44267-Birthcare-stable-baseline-beta-1' of https://github.com/Nictiz/Nictiz-testscripts into NICTIZ-35513-GZ-actualisatie-Kraam-casus-1
    
    * restructure folder tree and add buildall
    
    * add usecase property for loadscript differentiation
    
    * fix checkcontent property
    
    * update version
    
    * setup obstetric care casus 1
    
    * styling fixes
    
    * fix count related person
    
    * update id's en generate tests
    
    * fix authorization header baby and add request mode
    
    * test fixes
    
    * small fixes
    
    * small test fixes
    
    * test descriminator pregnancy duration
    
    * smal fix test descriminator pregnancy duration
    
    * matching the right pregnancy duration
    
    * update patient joris resource
    
    * update encounter query to IG
    
    * update date and datime elements
    
    * regenerate with fix in assert
    
    * regenerate with content assert fixt
    
    * NICTIZ-33768: regenerate with new testdata
    
    * delete out of scope resources
    
    * NICTIZ-33768: Regenerate with caption fix (local)
    
    * NICTIZ-33768: Regenerate 2 with local fix
    
    * update descriptions to be more user friendly
    
    * updated related persons XIS tests to atleast count asserts
    
    * NICTIZ-35514: update content type assert to include different image mime types
    
    * NICTIZ-33768 update fixture variable
    
    * add descriptions for all relevant check content asserts
    
    * update descriptions for alle relevant content assert to right format

--- buildscripts/BirthCare-complete.sh
+++ buildscripts/BirthCare-complete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ant -f ../src/build-multiple.xml -Dinput.dirs="src/BirthCare/4.0/PregnancyCard/Ultrasound/Test,src/BirthCare/4.0/PregnancyCard/MaternityCare/Test"
+ant -f ../src/build-multiple.xml -Dinput.dirs="src/BirthCare/4.0/PregnancyCard/Ultrasound/Test,src/BirthCare/4.0/PregnancyCard/MaternityCare/Test,src/BirthCare/4.0/PregnancyCard/ObstetricCare/Test"
