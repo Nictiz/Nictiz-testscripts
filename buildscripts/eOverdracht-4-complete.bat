caea39137354dd34031321720ee7586dd3f05941
lisaDekker-nictiz <lisa.dekker@nictiz.nl>
2026-09-04 12:05:49 +0200

    EOVDR-947 restore original test and cert folders

--- buildscripts/eOverdracht-4-complete.bat
+++ buildscripts/eOverdracht-4-complete.bat
@@ -1,6 +1,6 @@
 @setlocal enabledelayedexpansion
 @echo off
 
-call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/eOverdracht-4-0/Prevalidation,src/eOverdracht-4-0/Validation,src/eOverdracht-4-2/Prevalidation"
+call ant -f ..\src\build-multiple.xml -Dinput.dirs="src/eOverdracht-4-0/Prevalidation, src/eOverdracht-4-0/Validation, src/eOverdracht-4-0/Cert, src/eOverdracht-4-0/Test, src/eOverdracht-4-2/Prevalidation"
 
 pause
